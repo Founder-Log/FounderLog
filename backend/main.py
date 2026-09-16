eec66f14834a862a91f15f885527cbf4e9f3a43a
Aram Galstjan <galstanaram9@gmail.com>
2026-09-16 22:26:35 +0200

    feat(auth): setup JWT authentication logic and swagger configuration

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi import Depends
+from backend.core.auth import get_current_user
 
 from backend.core.config import settings
 from backend.core.registry import MODULES
